9138d9c07b1f12a966907b32f3ef135f4811f1ba
AlejaMontiel <167145236+AlejaMontiel@users.noreply.github.com>
2024-10-03 10:26:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
-# Configurar la barra lateral
 st.sidebar.title("Acerca de:")
-st.sidebar.write("En esta aplicación veremos la capacidad que ahora tiene una máquina de interpretar un boceto. ¡Dibuja cualquier cosa y ve lo que sucede!")
+st.sidebar.write("En esta aplicación veremos la capacidad que ahora tiene una máquina de interpretar un boceto.")
 
-# Configuración del grosor de la línea y herramienta de dibujo en la barra lateral
+# Sidebar components
 stroke_width = st.sidebar.slider('Selecciona el ancho de línea', 1, 30, 5)
 drawing_mode = st.sidebar.selectbox("Herramienta de dibujo:", ["freedraw", "line", "rect", "circle", "transform"])
 
-# Elegir color del trazo
+# Main interface
 st.write("### Color de Trazo")
+
 stroke_color = st.color_picker("Escoge el color", "#000000")
 
-# Crear un lienzo mucho más grande (1200x1200)
+# Create canvas
 canvas_result = st_canvas(
     stroke_width=stroke_width,
     stroke_color=stroke_color,
-    background_color="#FFFFFF",  # Fondo blanco
+    background_color="#FFFFFF",  # Background color white
     update_streamlit=True,
-    height=1200,  # Altura del lienzo
-    width=1200,   # Ancho del lienzo
+    height=700,
+    width=700,
     drawing_mode=drawing_mode,
     key="canvas",
 )
 
-# Botón para analizar la imagen
+# Input and button for image analysis
+clave = st.text_input("Ingresa tu Clave")
 if st.button("Analiza la imagen"):
-    st.write("La imagen ha sido procesada.")
-    # Lógica de análisis de imagen podría añadirse aquí
-
+    if clave:
+        st.write(f"La clave ingresada es: {clave}")
+        st.write("La imagen ha sido procesada.")
