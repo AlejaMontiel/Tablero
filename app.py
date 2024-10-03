c082e3aee34cccfd61cd44ce58e4f1af222f9ae0
AlejaMontiel <167145236+AlejaMontiel@users.noreply.github.com>
2024-10-03 10:12:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title('Tablero para dibujar')
 # Specify canvas parameters in application
 drawing_mode = "freedraw"
 stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
-stroke_color = '#FFFFFF' # Set background color to white
+stroke_color = '#FFFFFF'  # Set line color to white
 bg_color = '#000000'
 
 # Create a canvas component
@@ -21,8 +21,9 @@ canvas_result = st_canvas(
     key="canvas",
 )
 
+# Sidebar content
 st.sidebar.title("Acerca de:")
-st.sidebar.text("En este tablero puedes
-st.sidebar.text ("dibujar cualquier cosa")
-st.sidebar.text ("y la app la adivinará")
+st.sidebar.text("En este tablero puedes")
+st.sidebar.text("dibujar cualquier cosa")
+st.sidebar.text("y la app la adivinará")
 
